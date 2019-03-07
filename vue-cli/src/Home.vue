2cfac268e4f5236d523aa926dbc70c304c20bfe7
jedelson <edelson.m.jason@gmail.com>
2019-03-07 15:23:44 -0800

    using components
    import component from seperate vue file, us loop to display multiple components

--- vue-cli/src/Home.vue
+++ vue-cli/src/Home.vue
@@ -1,22 +1,14 @@
 <template>
-    <div>
-        <p>Server Status: {{ status }}</p>
-        <hr>
-        <button @click="changeStatus">Change Status</button>
-    </div>
+  <div>
+    <app-server-status v-for="server in 5" v-bind:key=server></app-server-status>
+  </div>
 </template>
 
 <script>
+import ServerStatus from './ServerStatus.vue';
 export default {
-  data: function () {
-    return{
-      status: 'Critical'
-    }
-  },
-  methods:{
-      changeStatus(){
-          this.status = 'Normal'
-      }
+  components: {
+    "app-server-status": ServerStatus
   }
 }
 </script>
