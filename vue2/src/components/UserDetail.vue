fe32f6ac621a67db407c3dba5e909aef9276ffd5
jedelson <edelson.m.jason@gmail.com>
2019-03-11 17:34:06 -0700

    using props between two components

---
+++ vue2/src/components/UserDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{names}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            names: [String, Array],
+            default:"Names"
+        },
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
